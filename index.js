30cd0de2072a592771517bc92a729ec26924bf11
saltine6 <saltines06@gmail.com>
2024-10-03 16:32:56 -0400

    made info cards work + added some icons

--- index.js
+++ index.js
@@ -20,36 +20,124 @@ const skillLeadershipCard = document.getElementById("leadershipCard");
 const skillGameDevelopmentCard = document.getElementById("gameDevelopmentCard");
 const skillAcademicsCard = document.getElementById("academicsCard");
 
+const overlayProgramming = document.getElementById("ProgrammingInfoOverlay");
+const overlayEditing = document.getElementById("EditingInfoOverlay");
+const overlayVideography = document.getElementById("VideographyInfoOverlay");
+const overlayPhotography = document.getElementById("PhotographyInfoOverlay");
+const overlayWebDesign = document.getElementById("WebDesignInfoOverlay");
+const overlayLeadership = document.getElementById("LeadershipInfoOverlay");
+const overlayGameDevelopment = document.getElementById("GameDevelopmentInfoOverlay");
+const overlayAcademics = document.getElementById("AcademicsInfoOverlay");
+
 skillProgrammingCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    overlayProgramming.style.display = "block";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillEditingCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "block";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillVideographyCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "block";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillPhotographyCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "block";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillWebDesignCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "block";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillLeadershipCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "block";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "none";
 })
 
 skillGameDevelopmentCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "block";
+    overlayAcademics.style.display = "none";
 })
 
 skillAcademicsCard.addEventListener('click', function() {
     infoOverlayContainer.style.display = "block";
+
+    infoOverlayContainer.style.display = "block";
+    overlayProgramming.style.display = "none";
+    overlayEditing.style.display = "none";
+    overlayVideography.style.display = "none";
+    overlayPhotography.style.display = "none";
+    overlayWebDesign.style.display = "none";
+    overlayLeadership.style.display = "none";
+    overlayGameDevelopment.style.display = "none";
+    overlayAcademics.style.display = "block";
 })
 
 skillsTab.addEventListener('click', function() {
